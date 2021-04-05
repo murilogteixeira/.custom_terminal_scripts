954702f0b7e36986132fcec1f5b9e4650d992a33
Murilo Teixeira <murilo.teixeira@btgpactual.com>
2021-04-05 09:50:33 -0300

    Update files

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+echo 'Atualizando scripts'
+git -C $HOME/.custom_terminal_scripts pull
+
 updtzsh() {
     source ~/.zshrc
     echo "Scripts updated"
@@ -9,28 +12,26 @@ alias cdgt="cd ~/git"
 alias gtst="git status"
 alias gtadd="git add --all"
 alias gtcmt="git commit"
+alias gtcmtm="git commit -m"
 alias gtpll="git pull"
 alias gtpsh="git push"
 alias gtftch="git fetch"
 
-gtcmtm() {
-    gtcmt -m $1
-}
-
-gtflw_option() {
-    case $2 in
-    -S) git flow $1 start $3;; 
-    -F) git flow $1 finish $3;; 
-    *) echo "[git flow $1] Parâmetro inválido ou incompleto";; 
-    esac
-}
-
 gtflw() {
+
+    __gtflw_option() {
+        case $2 in
+        -S) git flow $1 start $3;; 
+        -F) git flow $1 finish $3;; 
+        *) echo "[git flow $1] Parâmetro inválido ou incompleto";; 
+        esac
+    }
+    
     case $1 in
-    -f) gtflw_option feature $2 $3;;
-    -b) gtflw_option bugfix $2 $3;;
-    -r) gtflw_option release $2 $3;;
-    -h) gtflw_option hotfix $1 $2 $3;;
+    -f) __gtflw_option feature $2 $3;;
+    -b) __gtflw_option bugfix $2 $3;;
+    -r) __gtflw_option release $2 $3;;
+    -h) __gtflw_option hotfix $1 $2 $3;;
     *) echo '[git flow] Parâmetro inválido ou incompleto';;
     esac
 }
