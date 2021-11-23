764cbcc043476e98ce5346406458689e72832058
Murilo Teixeira <murilo.teixeira@btgpactual.com>
2021-11-23 12:37:50 -0300

    Fix video to gif converter

--- script.sh
+++ script.sh
@@ -148,19 +148,21 @@ gtflw() {
 # Criar GIF usando ffmpeg e gifsicle
 # brew install ffmpeg
 # brew install gifsicle
+# Como usar: gifly video.mp4
+# Resultado: video.mp4.gif
 
 gifly() {
     # obter largura e altura
     width=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 "$1") 
     height=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 "$1")
     # diminuir para 25% do tamanho
-    width=$(($width * 0.25))
-    height=$(($height * 0.25))
+    width=$(($width * 0.5))
+    height=$(($height * 0.5))
     # remover casas decimais
     width=${width%.*}
     height=${height%.*}
     # criar string no formato correto
     resolution="${width}x${height}"
     # converter video para gif
-    ffmpeg -i "$1" -s "$resolution" -pix_fmt rgb24 -r 20 -f gif -  | gifsicle --optimize=3 --delay=5 > "$2"
+    ffmpeg -i "$1" -s "$resolution" -pix_fmt rgb24 -r 20 -f gif -  | gifsicle --optimize=3 --delay=5 > "$1.gif"
 }
